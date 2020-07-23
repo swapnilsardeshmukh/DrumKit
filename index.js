3faae1374f1c921715d759f2ccf014349d90ae00
swapnilsardeshmukh <54372416+swapnilsardeshmukh@users.noreply.github.com>
2020-07-23 23:55:35 +0530

    minor letter change

--- index.js
+++ index.js
@@ -18,32 +18,32 @@ for (var i = 0; i < numberOfDrumButtons; i++)
 
           function makeSound(key){
             switch(key){
-                case "w":
+                case "s":
                 var tom1 = new Audio('sounds/tom-1.mp3');
                 tom1.play();
                 break;
 
-                case "a":
+                case "w":
                 var tom2 = new Audio('sounds/tom-2.mp3');
                 tom2.play();
                 break;
 
-                case "s":
+                case "a":
                 var tom3 = new Audio('sounds/tom-3.mp3');
                 tom3.play();
                 break;
 
-                case "d":
+                case "p":
                 var tom4 = new Audio('sounds/tom-4.mp3');
                 tom4.play();
                 break;
 
-                case "j":
+                case "n":
                 var snare = new Audio('sounds/snare.mp3');
                 snare.play();
                 break;
 
-                case "k":
+                case "i":
                 var kick = new Audio('sounds/kick-bass.mp3');
                 kick.play();
                 break;
